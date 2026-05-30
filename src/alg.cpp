0a7ba73afb36cd5809821b572e1cf4cda07e1dbc
Mirghit <yuandru25@gmail.com>
2026-05-30 20:54:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,27 +7,25 @@
 #include <iostream>
 #include <string>
 #include <utility>
+#include <vector>
 
 void makeTree(BST<std::string>& tree, const char *filename) {
     std::ifstream file(filename);
-    if (!file) {
-        std::cerr << "Не удалось открыть файл: " << filename << std::endl;
+    if (!file.is_open()) {
+        std::cout << "Не удалось открыть файл: " << filename << std::endl;
         return;
     }
     std::string word;
     char ch;
     while (file.get(ch)) {
-        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
-            if (ch >= 'A' && ch <= 'Z') {
-                ch = ch - 'A' + 'a';
-            }
-            word.push_back(ch);
+        if (std::isalpha(static_cast<unsigned char>(ch))) {
+          word += std::tolower(static_cast<unsigned char>(ch));
         } else {
-            if (!word.empty()) {
-                tree.insert(word);
-                word.clear();
-            }
-        }
+          if (!word.empty()) {
+            tree.insert(word);
+            word.clear();
+      }
+    }
     }
     if (!word.empty()) {
         tree.insert(word);
@@ -36,22 +34,17 @@ void makeTree(BST<std::string>& tree, const char *filename) {
 }
 
 void printFreq(BST<std::string>& tree) {
-    auto nodes = tree.collectNodes();   // добавлено
-    std::sort(nodes.begin(), nodes.end(),
-        [](const std::pair<std::string, int>& a,
-           const std::pair<std::string, int>& b) {
-            if (a.second != b.second) return a.second > b.second;
-            return a.first < b.first;
-        });
+    std::vector<std::pair<std::string, int>> fileWords = tree.collectNodes();
+    std::sort(fileWords.begin(), fileWords.end(),
+    [](const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) {
+      if (a.second != b.second) return a.second > b.second;
+      return a.first < b.first;
+    });
+
     std::ofstream outFile("result/freq.txt");
-    if (!outFile) {
-        std::cerr << "Не удалось открыть result/freq.txt" << std::endl;
-    }
-    for (const auto& p : nodes) {
-        std::cout << p.first << " - " << p.second << std::endl;
-        if (outFile) {
-            outFile << p.first << " - " << p.second << std::endl;
-        }
+    for (const auto& p : fileWords) {
+    std::cout << p.first << " -> " << p.second << std::endl;
+    outFile << p.first << " -> " << p.second << std::endl;
     }
     outFile.close();
 }
