5e2258138c041e52d56562399c59460fb75d6f9d
Mirghit <yuandru25@gmail.com>
2026-05-30 23:15:37 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,10 +1,12 @@
+// Copyright 2021 NNTU-CS
+
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
-#include <string>
-#include <vector>
-#include <utility>
 #include <algorithm>
+#include <string>
+#include <utility>
+#include <vector>
 
 template<typename T>
 class BST {
@@ -18,6 +20,7 @@ class BST {
   };
   Node* root;
   int len;
+
   void add(Node*& node, const T& k) {
     if (node == nullptr) {
       node = new Node(k);
@@ -32,34 +35,39 @@ class BST {
       node->freq++;
     }
   }
+
   int founder(Node* node, const T& k) const {
     if (node == nullptr) return 0;
     if (k == node->key) return node->freq;
     if (k < node->key) return founder(node->left, k);
     return founder(node->right, k);
   }
+
   int depthOfTree(Node* node) const {
     if (node == nullptr) return -1;
     int leftH = depthOfTree(node->left);
     int rightH = depthOfTree(node->right);
     return std::max(leftH, rightH) + 1;
   }
+
   void gathering(Node* node, std::vector<std::pair<T, int>>& vec) const {
     if (node == nullptr) return;
     gathering(node->left, vec);
     vec.push_back(std::make_pair(node->key, node->freq));
     gathering(node->right, vec);
   }
-  void remove(Node* node) {
+
+  void clear(Node* node) {
     if (node == nullptr) return;
-    remove(node->left);
-    remove(node->right);
+    clear(node->left);
+    clear(node->right);
     delete node;
   }
 
  public:
   BST() : root(nullptr), len(0) {}
-  ~BST() { remove(root); }
+  ~BST() { clear(root); }
+
   bool empty() const { return root == nullptr; }
   void insert(const T& k) { add(root, k); }
   int depth() const { return depthOfTree(root); }
