57c084d24c097805ba963a743f6c9d255475d61d
Mirghit <yuandru25@gmail.com>
2026-05-30 20:47:42 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <vector>
 #include <utility>
+#include <string>
 
 template<typename T>
 class BST {
@@ -18,34 +19,36 @@ class BST {
   };
 
   Node *root;
+  int len;
 
-  void insert(Node*& node, const T& key) {
+  void insert(Node*& node, const T& val) {
     if (node == nullptr) {
       node = new Node(key);
+      len++;
       return;
     }
     if (key == node->key) {
       node->freq++;
     } else if (key > node->key) {
-      insert(node->right, key);
+      insert(node->right, val);
     } else {
-      insert(node->left, key);
+      insert(node->left, val);
     }
   }
 
   bool search(Node* node, const T& val) const {
     if (node == nullptr) return false;
-    if (val == node->key) return true;
+    if (val == node->key) return node->freq;
     if (val > node->key) return search(node->right, val);
     return search(node->left, val);
   }
 
   int depth(Node *node) const {
-    if (node == nullptr) return 0;
+    if (node == nullptr) return -1;
     return 1 + std::max(depth(node->left), depth(node->right));
   }
 
-  void gathering(Node *node, std::vector<std::pair<T, int>> &vect) const {
+  void gathering(Node *node, std::vector<std::pair<T, int>>& vect) const {
     if (node == nullptr) return;
     gathering(node->left, vect);
     vect.push_back(std::make_pair(node->key, node->freq));
@@ -60,12 +63,18 @@ class BST {
   }
 
  public:
-  BST() : root(nullptr) {}
+  BST() : root(nullptr), len(0) {}
   ~BST() { clear(root); }
 
-  void insert(const T& key) { insert(root, key); }
+  void insert(const T& key) { insert(root, val); }
   bool search(const T& val) const { return search(root, val); }
   int depth() const { return depth(root); }
+  int size() const {
+    return len ;
+  }
+  bool empty() const {
+    return root == nullptr;
+  }
 
   std::vector<std::pair<T, int>> collectNodes() const {
     std::vector<std::pair<T, int>> res;
